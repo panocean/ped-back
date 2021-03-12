93b5650840786dbef79849b7a9b4b718df31fda6
jidemobell <jidd32@gmail.com>
2021-03-12 01:04:11 +0100

    alter redis config

--- db/redis/redis.js
+++ db/redis/redis.js
@@ -1,5 +1,13 @@
 const redis = require("async-redis");
-const redisClient = redis.createClient(process.env.REDIS_PORT)
+// const redisClient = redis.createClient(process.env.REDIS_PORT)
+
+let redisClient
+if(process.env.REDISCLOUD_URL){
+    let redisURL = url.parse(process.env.REDISCLOUD_URL);
+    redisClient = redis.createClient(redisURL)
+} else {
+    redisClient = redis.createClient(process.env.REDIS_PORT)
+}
 
 module.exports = { redisClient }
 
